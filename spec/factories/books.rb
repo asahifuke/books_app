fd2cdf2c0429c52f51123bdece6f673b9eb3b07e
asahifuke <asahi.fuke@medpeer.co.jp>
2022-08-20 07:16:04 +0900

    リアルなテストコードを作成

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,9 +2,9 @@
 
 FactoryBot.define do
   factory :book do
-    title { 'test title' }
-    memo { 'test memo' }
-    author { 'test auther' }
+    title { 'プロを目指す人のためのRuby入門 言語仕様からテスト駆動開発・デバッグ技法まで' }
+    memo { 'Rubyの文法をサンプルコードで学び、例題でプログラミングの流れを体験できる解説書です。' }
+    author { '伊藤 淳一' }
   end
 
   trait :with_attachment do
--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -7,46 +7,46 @@ RSpec.describe 'Books', type: :system do
 
   it '一覧画面のテスト' do
     visit books_path
-    expect(page).to have_content 'test title'
+    expect(page).to have_content 'プロを目指す人のためのRuby入門 言語仕様からテスト駆動開発・デバッグ技法まで'
   end
 
   it '新規作成のテスト' do
     visit books_path
     click_link 'New Book'
-    fill_in 'Title', with: 'create title test'
-    fill_in 'Memo', with: 'create memo test'
-    fill_in 'Author', with: 'create author test'
+    fill_in 'Title', with: '新しいLinuxの教科書'
+    fill_in 'Memo', with: '入門書'
+    fill_in 'Author', with: '三宅 英明'
     attach_file 'Picture', Rails.root.join('spec/files/attachment.jpg')
     click_button 'Create Book'
     expect(page).to have_content 'Book was successfully created.'
-    expect(page).to have_content 'create title test'
-    expect(page).to have_content 'create memo test'
-    expect(page).to have_content 'create author test'
-    expect(page).to have_selector("img[src$='attachment.jpg']")
+    expect(page).to have_content '新しいLinuxの教科書'
+    expect(page).to have_content '入門書'
+    expect(page).to have_content '三宅 英明'
+    expect(page).to have_selector "img[src$='attachment.jpg']"
   end
 
   it '編集のテスト' do
     visit books_path
     click_link 'Edit', match: :first
-    fill_in 'Title', with: 'update title test'
-    fill_in 'Memo', with: 'update memo test'
-    fill_in 'Author', with: 'update author test'
+    fill_in 'Title', with: 'Everyday Rails - RSpecによるRailsテスト入門'
+    fill_in 'Memo', with: 'RSpecを使ってRailsアプリケーションに信頼性の高いテストを書く実践的なアドバイスを提供します。'
+    fill_in 'Author', with: 'Aaron Sumner and Junichi Ito'
     attach_file 'Picture', Rails.root.join('spec/files/attachment.jpg')
     click_button 'Update Book'
     expect(page).to have_content 'Book was successfully updated.'
-    expect(page).to have_content 'update title test'
-    expect(page).to have_content 'update memo test'
-    expect(page).to have_content 'update author test'
-    expect(page).to have_selector("img[src$='attachment.jpg']")
+    expect(page).to have_content 'Everyday Rails - RSpecによるRailsテスト入門'
+    expect(page).to have_content 'RSpecを使ってRailsアプリケーションに信頼性の高いテストを書く実践的なアドバイスを提供します。'
+    expect(page).to have_content 'Aaron Sumner and Junichi Ito'
+    expect(page).to have_selector "img[src$='attachment.jpg']"
   end
 
   it '削除のテスト' do
     visit books_path
-    expect(page).to have_content 'test title'
+    expect(page).to have_content 'プロを目指す人のためのRuby入門 言語仕様からテスト駆動開発・デバッグ技法まで'
     page.accept_confirm do
       click_link 'Destroy', match: :first
     end
     expect(page).not_to have_content 'Book was successfully destroy.'
-    expect(page).not_to have_content 'test title'
+    expect(page).not_to have_content 'プロを目指す人のためのRuby入門 言語仕様からテスト駆動開発・デバッグ技法まで'
   end
 end
